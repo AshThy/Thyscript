4912e8c28b7ac7dd60fd46f51ef5ee218a94199e
AshThy <ashethay221099@gmail.com>
2017-05-19 19:00:45 -0300

    Atualizado

--- Orianna - Toy/Orianna - Toy/Spells.cs
+++ Orianna - Toy/Orianna - Toy/Spells.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using EloBuddy.SDK;
+using EloBuddy;
+using EloBuddy.SDK.Enumerations;
 
 namespace Orianna___Toy
 {
     internal class Spells
     {
+        public static Spell.Skillshot Q;
+        public static Spell.Active W, R;
+        public static Spell.Targeted E;
+
         internal static void Iniciar()
         {
-            throw new NotImplementedException();
+            Q = new Spell.Skillshot(SpellSlot.Q, 825, SkillShotType.Circular, 1, 1200, 175)
+            {
+                AllowedCollisionCount = -1,
+            };
+            W = new Spell.Active(SpellSlot.W);
+            E = new Spell.Targeted(SpellSlot.E, 1100);
+            R = new Spell.Active(SpellSlot.R);
         }
     }
 }
