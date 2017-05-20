c429193dd89b60e734dde3c8d3b16ef9daef3b8c
AshThy <ashethay221099@gmail.com>
2017-05-20 18:50:10 -0300

    Experimental....

---
+++ Riven - Exilada/Riven - Exilada/Riven.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+
+namespace Riven___Exilada
+{
+    class Riven
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += OnGameComplete;
+        }
+
+        private static void OnGameComplete(EventArgs args)
+        {
+            if (Player.Instance.ChampionName != "Riven")
+            {
+                return;
+            }
+
+        }
+    }
+}
